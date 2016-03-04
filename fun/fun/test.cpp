5922692be5045a97e626ed215b5374e7f75e3bf2
edwin <sblig@126.om>
2016-03-04 23:14:12 +0800

    慕课网联系

--- fun/fun/test.cpp
+++ fun/fun/test.cpp
@@ -1,36 +1,45 @@
-#include <stdlib.h>
 #include <iostream>
 using namespace std;
+/**
+*函数功能：返回a和b的最大值
+*a和b是两个整数
+*/
+int getMax(int a, int b)
+{
+	return a > b ? a : b;
+}
 
-// inline 内联函数
+/**
+* 函数功能：返回数组中的最大值
+* arr：整型数组
+* count：数组长度
+* 该函数是对上面函数的重载
+*/
+int getMax(int arr[], int count)
+{
+	//定义一个变量并获取数组的第一个元素
+	int maxNum = arr[0];
+	for (int i = 1; i < count; i++)
+	{
+		//比较变量与下一个元素的大小
+		if (maxNum < arr[i])
+		{
+			//如果数组中的元素比maxNum大，则获取数组中的值
+			maxNum = arr[i];
+		}
+	}
+	return maxNum;
+}
 
-// 函数默认值
-inline void fun(int i=30, int j=20, int k=10);
+int main(void)
+{
+	//定义int数组并初始化
+	int numArr[3] = { 3, 8, 6 };
 
-//函数重载
-inline void fun(double i, double j);
+	//自动调用int getMax(int a, int b)
+	cout << getMax(1, 2) << endl;
 
-int main(void) {
-	// 函数默认值
-	fun();
-	fun(100);
-	fun(100, 200);
-	fun(100, 200, 300);
-
-	//函数重载
-	fun(1.1, 2.2); // 重载 
-	fun(1, 2);     // 调用函数默认值
-	//fun(1.1, 2); // 报错
-
-
-	system("pause");
+	//自动调用返回数组中最大值的函数返回数组中的最大
+	cout << getMax(numArr, 3) << endl;
 	return 0;
-}
-
-void fun(int i/* =30 */, int j/* =20 */, int k/* =10 */) {
-	cout << i << "," << j << "," << k << endl;
-}
-
-void fun(double i, double j) {
-	cout << i << "," << j << endl;
 }
